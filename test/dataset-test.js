778a289b52ed698abcb604b2f56f3376aa181c54
Alex Ford <alexford@frontside.io>
2015-11-12 16:27:37 -0600

    Remove skeleton tests which have been implemented

--- test/dataset-test.js
+++ test/dataset-test.js
@@ -797,26 +797,5 @@ describe("Dataset", function() {
         });
       });
     });
-
-    describe.skip("with no fetch function", function() {
-      it("emits an observation of the state");
-      it("indicates that the dataset is not doing any loading");
-    });
-
-    describe.skip("with a fetch function and the default load horizon", function() {
-      it("requests the first page");
-      it("now has a requested page");
-      it("indicates that the dataset is now loading");
-      it("indicates that the first page is loading");
-      describe("when the first page resolves", function() {
-        it("integrates the statistics");
-        it("reflects the total number of records");
-        it("reflects the total number of pages");
-        it("indicates that the dataset is no longer loading");
-        it("indicates that the page is no longer loading");
-        it("contains empty objects for the items that have not even been requested");
-        it("contains unequested pages for the pages that have not been requested");
-      });
-    });
   });
 });
